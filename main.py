813ac73d55024ca96147282a5a5f76e2b228962c
Andrey Chernenko <xchernen@mendelu.cz>
2023-09-03 09:21:35 +0200

    footer

--- main.py
+++ main.py
@@ -3,15 +3,27 @@ import pandas as pd
 
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
-
+pdf.set_auto_page_break(False)
 data = pd.read_csv('topics.csv')
 
 for index, row in data.iterrows():
     pdf.add_page()
 
     pdf.set_font(family='Arial', style='', size=12)
+    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=10, txt=f'{row["Order"]}.{row["Topic"]}', ln=1)
     pdf.line(10, 22, 200, 22)
+    pdf.ln(265)
+
+    pdf.set_font(family='Arial', style='', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=f'{row["Topic"]}', ln=1, align='R')
+
     for i in range(row['Pages'] - 1):
         pdf.add_page()
+
+        pdf.ln(265)
+        pdf.set_font(family='Arial', style='', size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=f'{row["Topic"]}', ln=1, align='R')
 pdf.output('o.pdf')
